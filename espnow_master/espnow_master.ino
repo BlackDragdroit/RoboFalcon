021fd1086a2075982b6daef928bcb3d7c954d648
BlackDragDroit <gabrielgeyrhofer@gmail.com>
2026-03-03 13:48:43 +0100

    espnow_master and espnow_slave example code

--- espnow_master/espnow_master.ino
+++ espnow_master/espnow_master.ino
@@ -211,12 +211,10 @@ void sendData() {
 }
 
 // callback when data is sent from Master to Slave
+// Updated callback for newer ESP32 Core versions
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
-  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // Try changing 'const uint8_t *mac_addr' to 'const uint8_t *mac' 
+  // or simply casting it if the compiler is being picky about the internal typedef.
 }
 
 void setup() {
